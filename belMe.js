2df6400bb57f32c74eb8b280a546123d0737fa53
M Deco <mdecorte@gmail.com>
2014-10-29 14:41:44 +0100

    bugfix
    disappearing slider when clicking 2 times problem solved

--- belMe.js
+++ belMe.js
@@ -1,28 +1,28 @@
 
 	
 
-
-
-
-var pos = false;
+var pos = false
 
 // ALS JE KLIKT OP LABEL (belInUit)
 $("#belMeInUit").click(function(e) {
 	e.preventDefault();
 	if(pos){ // AlS MENU AL UIT IS GA 100PX NAAR LINKS
+		pos = false;
+
 		$("#belMe,#belMeInUit").animate({
 			left: "-=100px"
 		  }, 1200, "easeInOutCubic", function() {
-		  	$("#belMe").hide();
-		  	pos = false;
+		  	//$("#belMe").hide();
+		  	
 		  });				
 	} 
 	else { // ALS MENU NOG NIET UIT IS GA 100PX NAAR RECHTS
-		$("#belMe").show(); 
+		pos = true;
+		//$("#belMe").show(); 
 		$("#belMe,#belMeInUit").animate({
 			left: "+=100px"
 		  }, 1200, "easeInOutCubic");
-		pos = true;
+		
 	}
 
 		 });
